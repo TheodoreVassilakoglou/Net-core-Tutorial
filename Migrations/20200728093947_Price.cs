5cea950f8a2fa84e2b091a2d9f2332d7cf11429d
TheodoreVassilakoglou <theobear@live.com.au>
2020-07-29 20:50:34 +0930

    9-10 NET.CORE Signed-off-by: TheodoreVassilakoglou <theobear@live.com.au>

---
+++ Migrations/20200728093947_Price.cs
@@ -0,0 +1,17 @@
+﻿Ausing Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Price : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
